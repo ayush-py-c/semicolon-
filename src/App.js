3e409e25811c0db21a5190610b5275317927c1d6
Ayushraj_2804 <85986862+ayush-py-c@users.noreply.github.com>
2024-12-15 02:49:57 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import FalloutSurvivalNetwork from './appy';
+import GlowingButton from './glowbutton';
+
+ const App = () => {
+  return (
+    <div className='bg-black no-scrollbar'>
+       <div className='fixed z-50 scale-[25%] left-[29.3rem] top-96'>
+        <GlowingButton ClickEvent={()=>alert("Ass")}/>
+       </div>
+        <img className='w-screen pointer-events-none fixed z-40 h-screen object-fit object-[0%_-0%] bg-' src='bg.png'></img>
+    <div className='scale-[60%] translate-x-9 -translate-y-9'>
+        <FalloutSurvivalNetwork/>
+    </div>
+    </div>
+    
+  )
+}
+export default App;
